fc8f1fe03ee271372ed83dbdd2deb0eae79edd1e
doshest <doshest@gmail.com>
2017-05-23 15:05:48 +0800

    ff

--- src/main/java/org/springframework/samples/mvc/mapping/MappingController.java
+++ src/main/java/org/springframework/samples/mvc/mapping/MappingController.java
@@ -1,13 +1,13 @@
 package org.springframework.samples.mvc.mapping;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MappingController {
@@ -59,6 +59,10 @@ public class MappingController {
 
 	@RequestMapping(value="/mapping/produces", method=RequestMethod.GET, produces=MediaType.APPLICATION_XML_VALUE)
 	public @ResponseBody JavaBean byProducesXml() {
+		return new JavaBean();
+	}
+	@RequestMapping(value="/mapping/produces", method=RequestMethod.GET, produces=MediaType.APPLICATION_XML_VALUE)
+	public @ResponseBody JavaBean byProducesXml1111() {
 		return new JavaBean();
 	}
 
